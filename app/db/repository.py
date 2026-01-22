6d042331c94c817f59d78281b0645c25a11a4d60
Lucas Moreira <lucas.moreira@CELLAVITABRASIL.local>
2026-01-22 16:24:41 -0300

    refactor: convertendo repository para async

--- app/db/repository.py
+++ app/db/repository.py
@@ -1,29 +1,42 @@
-from app.db.models import db, Item
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.models import Item
 from app.logger import logger
 
-def upsert_item(codigo, descricao, lote, status, validade):
+
+async def upsert_item(db: AsyncSession, codigo: str, descricao: str, lote: str, status: str, validade):
     try:
-        # 1. Procuramos se já existe esse par Codigo/Lote
-        item = Item.query.filter_by(codigo=codigo, lote=lote).first()
+        # 1. Busca Assíncrona (Substitui o Item.query.filter_by)
+        # "Selecione o Item onde Codigo é igual a X e Lote é igual a Y"
+        stmt = select(Item).where(Item.Codigo == codigo, Item.Lote == lote)
+        
+        # Executa a query no banco esperando a resposta (await)
+        result = await db.execute(stmt)
+        item = result.scalars().first()
 
         if item:
             # 2. Se existe, atualizamos (UPDATE)
-            item.descricao = descricao
-            item.status = status
-            item.validade = validade
+            item.Descricao = descricao
+            item.Status = status
+            item.Validade = validade
         else:
             # 3. Se não existe, criamos um novo (INSERT)
             novo_item = Item(
-                codigo=codigo,
-                descricao=descricao,
-                lote=lote,
-                status=status,
-                validade=validade
+                Codigo=codigo,
+                Descricao=descricao,
+                Lote=lote,
+                Status=status,
+                Validade=validade
             )
-            db.session.add(novo_item)
+            db.add(novo_item) # Adiciona na memória da sessão
         
-        db.session.commit() # Salva de verdade
+        # 4. Commit para salvar as mudanças no banco
+        await db.commit()
+        
+        return item
+        
     except Exception as e:
-        db.session.rollback() # Se der erro, desfaz tudo
+        # Se der erro, desfaz tudo
+        await db.rollback()
         logger.error(f"Erro no upsert: {e}")
-        raise
+        raise e
