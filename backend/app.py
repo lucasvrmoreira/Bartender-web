72afb63988a1a8ab515fb4a15066d92650e0c989
Lucas Moreira <lucasvrmoreira@gmail.com>
2026-02-25 13:20:50 -0300

    fix: remove sentry and update start logic

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,17 @@
-
-import sentry_sdk
-from sentry_sdk.integrations.flask import FlaskIntegration
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-
-
 from backend.config import Config 
 from backend.routes.api import api_bp
 from backend.routes.print import print_bp
-from backend.logger import logger
 from backend.db.models import db 
-
-
-
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-
 app.config.from_object(Config)
 
+# Inicialização do DB
 db.init_app(app)
 
+# Registre as rotas
 app.register_blueprint(api_bp)
 app.register_blueprint(print_bp)
-
-
